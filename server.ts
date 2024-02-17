e76f0330e216ca172633f001035652df6f93d6eb
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-02-16 21:26:07 -0300

    change json-server version

--- server.ts
+++ server.ts
@@ -1,10 +1,10 @@
 import { create, router as jsonRouter, defaults } from "json-server";
 const server = create();
-const router = jsonRouter({ source: "db.json" });
+const router = jsonRouter("db.json");
 const middlewares = defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
+server.listen(5559, () => {
   console.log("JSON Server is running");
 });
